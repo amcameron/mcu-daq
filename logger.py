9fd2abb42ce44f2ab3c23ed333189a06d42ca1c5
Andrew Cameron <amcameron@gmail.com>
2010-03-06 23:44:12 -0500

    add Windows serial port creation and read for a length of time

--- logger.py
+++ logger.py
@@ -1,8 +1,18 @@
 # note: requires pySerial (http://pyserial.sourceforge.net/)
-
 import serial
-ser = serial.Serial('/dev/tty.usbserial', 9600)
-while 1:
-	ser.readline()
+from time import time
+
+# Linux:
+#ser = serial.Serial('/dev/tty.usbserial', 9600, timeout = 0)
+# Windows:
+ser = serial.Serial('COM3', 9600, timeout = 0)
+
+now = time()
+ser.write('B')
+while time() - now < 5.0:
+	if ser.inWaiting():
+		print ser.readline()
+
+ser.write('E')
 
 #ta-da!  maybe make it write to a file, next.
